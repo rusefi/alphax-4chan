eab8cf4cdac0fa0525531db962e18965306708e8
andreika-git <andrei.home@gmail.com>
2022-04-27 16:26:42 +0300

    update rev.B

--- step2_copy_alphax_4ch.sh
+++ step2_copy_alphax_4ch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd hellen-one
-python2.7 ./bin/copy_from_Kicad.py "frames:alphax_" ".." "../../gerber" "4ch" "a"
+python2.7 ./bin/copy_from_Kicad.py "frames:alphax_" ".." "../../gerber" "4ch" "b"
 
 echo "Done!"
--- step3_create_alphax_4ch.sh
+++ step3_create_alphax_4ch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd hellen-one
-sh bin/create_board_with_prefix.sh "alphax_" ".." "4ch" "a" "bom_replace_alphax-4ch-a.csv " "0,0"
+sh bin/create_board_with_prefix.sh "alphax_" ".." "4ch" "b" "bom_replace_alphax-4ch-b.csv " "0,0"
