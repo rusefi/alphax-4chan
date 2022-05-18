9db326ea474b90fefb938f4e1ee16ef2b89bf89d
rusefillc <sdfsdfqsf2334234234>
2022-05-18 09:01:06 -0400

    fresh

--- step2_copy.sh
+++ step2_copy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd hellen-one
-python2.7 ./bin/copy_from_Kicad.py "frames:alphax_" ".." "../../gerber" "4ch" "b"
+python3 ./bin/copy_from_Kicad.py "frames:alphax_" ".." "../../gerber" "4ch" "b"
 
 echo "Done!"
