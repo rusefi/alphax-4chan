942c01c791bfff79b2dd8de7504bd60a11c01c72
rusefillc <sdfsdfqsf2334234234>
2022-05-21 10:30:26 -0400

    progress

--- step3_create_board.sh
+++ step3_create_board.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+if [ ! -f hellen-one/git_scripts ]; then
+    echo "No submodules?"
+    git submodule update --init --recursive
+fi
+
 cd hellen-one
 sh bin/create_board_with_prefix.sh "alphax_" ".." "4ch" "b" "bom_replace_alphax-4ch-b.csv " "0,0"
